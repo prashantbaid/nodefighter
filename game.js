a92d124673997c805d5942ffb80383aa5cb71b8b
Prashant Baid <prashantvbaid@gmail.com>
2015-10-10 18:41:06 +0530

    game logic almost complete, next step: implement levels and score

--- game.js
+++ game.js
@@ -23,38 +23,86 @@ if (typeof process.stdin.setRawMode == 'function') {
 process.stdin.resume();
 
 
-var arr =[".", "o", "O", "o"];
 var lines = "  ||  ";
-var lspace = "   ";
-var space = "   ";
-var car = "[]";
-var noCar = "  ";
-var arena = '';
-
-
-function drawArena() {
-for(var i = 0; i <10; i++) {
-    if(i == 7) {
-      if(direction === 'left')
-        arena += lines + car + space + lines + '\n';
-      else
-        arena += lines + space + car + lines + '\n'; 
-    }
-  	 
-    else
-     arena += lines +  noCar + space +  lines + '\n';
+var space = "    ";
+var car = " [] ";
+var circuit = '';
+var carPos = 0;
+var compCar = [" [] ", "    "];
+var animationCounter = 0;
+var circuitBuilder = [];
+var toggle = false;
+function initializeCircuit() {
+  for(var i = 0; i <10; i++) {
+    circuitBuilder[i] = lines + space + space + lines + '\n';
+    circuit += circuitBuilder[i];  
   }
-  return arena;
+  return circuit;
+}
+
+//var arenaBluePrint = lines + compCar[0] + compCar[1] + lines + '\n';
+
+function launchCar(animationCounter) {
+  var circuitBlock;
+  circuitBuilder.pop();
+
+ // circuitBuilder.pop();
+ // circuitBuilder.pop();
+ if(animationCounter == 2)
+    circuitBlock = lines + compCar[0] + compCar[1] + lines + '\n';
+  else
+    circuitBlock = lines + space + space + lines + '\n';
+ // circuitBuilder.unshift(lines + space + space + lines + '\n')
+  circuitBuilder.unshift(circuitBlock);
+ // circuitBuilder.unshift(lines + space + space + lines + '\n');
+}
+
+function drawCircuit() {
+
+  
+
+  for(var i = 0; i <10; i++) {
+    if(i == 7) {
+          if(direction === 'left')
+            circuit += lines + car + space + lines + '\n';
+          else
+            circuit += lines + space + car + lines + '\n'; 
+        }
+    circuit += circuitBuilder[i];
+  }
+  //circuit = circuitBuilder.toString();
+  //carPos = i;
+    
+  //   if(i == carPos) 
+  //     circuit += lines + compCar[0] + compCar[1] + lines + '\n';	 
+  //   else
+  //     circuit += lines +  noCar + space +  lines + '\n';
+  // }
+  return circuit;
 }
 
 
 (function()
-        {
+        {   process.stdout.write('\033c');
+            console.log(initializeCircuit());
             setInterval(function() {
-            arena = '';  
+              animationCounter++;
+              if(animationCounter > 5) {
+                toggle = !toggle;
+                animationCounter = 0;
+              }
+              launchCar(animationCounter);
+                
+
+              shuffle(compCar);
+              // }
+                
+              // else
+              //   carPos++;
+              circuit = '';  
 
               process.stdout.write('\033[2f');
-              console.log(drawArena()); 
+              console.log(drawCircuit()); 
             }, 100 );
         })(); 
 
@@ -62,5 +110,25 @@ function getDirection(dir) {
    
   direction = dir;
 
+}
+
+function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
 }
 
